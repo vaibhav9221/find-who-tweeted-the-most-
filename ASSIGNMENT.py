3ca4096ccbdb1076c3f5662762c8f9f9b9f71eaa
vaibhav9221 <59912875+vaibhav9221@users.noreply.github.com>
2020-10-18 12:52:45 -0700

    Add files via upload

---
+++ ASSIGNMENT.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+tweet_names = ["sachin tweet_id_1",
+               "sehwag tweet_id_2",
+               "sachin tweet_id_3",
+               "sachin tweet_id_4"]
+
+uniq_names = [pref_names.split()[0] for
+              pref_names in tweet_names]
+
+times = Counter(uniq_names)
+repeat = times.values()
+
+for element in set(repeat):
+    dupl = ([(key, value) for
+             key, value in sorted(times.items()) if
+             value == element])
+
+    if len(dupl) > 1:
+        for (key, value) in dupl:
+            print(key, '', value)
+    max_value = max(times.values())
+    temp_max_result = [(key, value) for
+                       key, value in sorted(times.items()) if
+                       value == max_value]
+
+    if temp_max_result != dupl:
+        for (key, value) in temp_max_result:
+            print(key, '', value)
